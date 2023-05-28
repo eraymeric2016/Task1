a396ee2b0c5af4188ef5921793dd8961e247afd2
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:10:19 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,29 @@
+package week_03;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        /*
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
+         */
+
+
+        System.out.println("******************************");
+
+        System.out.print("Adam\n");
+        System.out.print("Eyup");
+
+        System.out.println("");
+        System.out.println("******************************");
+        System.out.print("Adam\tEyup\n");
+        System.out.println("Computer\\Laptop");
+        System.out.println("Cars(\"Mercedes\")");
+        System.out.println("*\n* *\n* * *\n* * * *");
+
+
+    }
+}
